482cd2402e2c00798741bf38618735393b221687
maheshnnaik <maheshnnaik07@gmail.com>
2023-12-17 23:05:25 +0530

    update styles

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -12,19 +12,33 @@ export const Content = () => {
 			<div className="top-container">
 				<div className="group1">
 					<div className="bell"></div>
-					<h4 className="top-text1">Get notified when a highly correlated whale makes a move</h4>
-					<div className="top-text2">Find out when a certain whale moves more than any preset amount on-chain or when a dormant whale you care about becomes active.</div>
+					<h4 className="top-text1">
+						Get notified when a highly correlated whale makes a move
+					</h4>
+					<div className="top-text2">
+						Find out when a certain whale moves more than any preset amount
+						on-chain or when a dormant whale you care about becomes active.
+					</div>
 				</div>
 				<div className="group2">
-					<Card /> 
+					<Card />
 				</div>
 			</div>
 			<div className="middle-container">
-				<img src={Cohorts} alt="Cohort" className="cohorts" />
-				<img src={Eye} alt="eye" className="eye" />
-				<div className="m-text">
-					<p className="m-text1">Watch what the whales are doing</p>
-					<p className="m-text2">All whales are not equal. Know exactly what the whales impacting YOUR portfolio are doing.</p>
+				<div className="cohort-img">
+					<img src={Cohorts} alt="Cohort" className="cohorts" />
+				</div>
+				<div className="m-right">
+					<div className="eye-image">
+						<img src={Eye} alt="eye" className="eye" />
+					</div>
+					<div className="m-text">
+						<p className="m-text1">Watch what the whales are doing</p>
+						<p className="m-text2">
+							All whales are not equal. Know exactly what the whales impacting
+							YOUR portfolio are doing.
+						</p>
+					</div>
 				</div>
 			</div>
 			<div className="bottom-container">
@@ -42,5 +56,5 @@ export const Content = () => {
 				</div>
 			</div>
 		</div>
-	)
+	);
 }
